b48136c08bb9b15c3885901bf0f1f7f124e9fc5d
Kreik2809 <200098@umons.ac.be>
2022-03-28 19:32:34 +0200

    thread behavior files + manette

--- raspsoft/src/raspsoft.py
+++ raspsoft/src/raspsoft.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
+import thread_soft as threads
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
@@ -57,11 +59,15 @@ class State():
 
 class Forward(State):
 
-    def __init__(self):
+    def __init__(self,speed):
         self.pwm_freq = 100
-        self.speed = 70
+        self.speed = speed
 
     def run(self):
+        self.thread = threads.thread_with_trace(target = self.thread_start)
+        self.thread.start()
+
+    def thread_start(self):
         pwm_cc.ChangeFrequency(self.pwm_freq)
         GPIO.output(13,1)
         GPIO.output(15,0)
@@ -69,17 +75,22 @@ class Forward(State):
 
     def stop(self):
         pwm_cc.stop()
-        GPIO.output(13, 0)
+        GPIO.output(13, 1)
         GPIO.output(15, 0)
+        self.thread.kill()
         
 
 class Backward(State):
 
-    def __init__(self):
+    def __init__(self,speed):
         self.pwm_freq = 100
-        self.speed = 40
+        self.speed = speed
 
     def run(self):
+        self.thread = threads.thread_with_trace(target = self.thread_start)
+        self.thread.start()
+    
+    def thread_start(self):
         pwm_cc.ChangeFrequency(self.pwm_freq)
         GPIO.output(13,0)
         GPIO.output(15,1)
@@ -89,52 +100,64 @@ class Backward(State):
         pwm_cc.stop()
         GPIO.output(13, 0)
         GPIO.output(15 ,1)
+        self.thread.kill()
 
 class TurnRight(State):
     
-    def __init__(self):
-        pass
+    def __init__(self,target_angle):
+        self.target_angle = target_angle
 
     def run(self):
-        pwm_servo.start(angle_to_percent(30))
+        self.thread = threads.thread_with_trace(target = self.thread_start)
+        self.thread.start()
+
+    def thread_start(self):
+        if(self.target_angle > 90 or self.target_angle < 30):
+            raise Exception("Target angle must be between 30 and 90 for TurnRight")
+        return_angle = 90
+        while(return_angle > self.target_angle):
+            pwm_servo.start(angle_to_percent(return_angle))
+            time.sleep(0.02)
+            return_angle -= 1
 
     def stop(self):
+        self.thread.kill()
         pwm_servo.ChangeDutyCycle(angle_to_percent(90))
         time.sleep(0.01)
 
 class TurnLeft(State):
 
-    def __init__(self):
-        pass
+    def __init__(self,target_angle):
+        self.target_angle = target_angle
 
     def run(self):
-        pwm_servo.start(angle_to_percent(150))
+        self.thread = threads.thread_with_trace(target = self.thread_start)
+        self.thread.start()
+
+    def thread_start(self):
+        if(self.target_angle <= 90 or self.target_angle >= 150):
+            raise Exception("Target angle must be between 90 and 150 for TurnLeft")
+        return_angle = 90
+        while(return_angle < self.target_angle):
+            pwm_servo.start(angle_to_percent(return_angle))
+            time.sleep(0.02)
+            return_angle += 1
 
     def stop(self):
+        self.thread.kill()
         pwm_servo.ChangeDutyCycle(angle_to_percent(90))
         time.sleep(0.01)
     
 if __name__ == "__main__":
     car = Car()
-    backward = Backward()
-    forward = Forward()
-    turnRight = TurnRight()
-    turnLeft = TurnLeft()
+    backward = Backward(60)
+    forward = Forward(60)
+    turnRight = TurnRight(30)
+    turnLeft = TurnLeft(150)
     car.add_state(forward)
-    time.sleep(1)
-    car.remove_state(forward)
-    car.add_state(backward)
-    time.sleep(1)
-    car.remove_state(backward)
+    time.sleep(2)
     car.add_state(turnRight)
-    car.add_state(forward)
+    time.sleep(0.5)
+    car.remove_state(forward)
     time.sleep(1)
     car.remove_state(turnRight)
-    car.add_state(turnLeft)
-    time.sleep(1)
-    car.remove_state(turnLeft)
-    car.remove_state(forward)
-    pwm_servo.stop()
-    pwm_cc.stop()
-    GPIO.cleanup()
-
